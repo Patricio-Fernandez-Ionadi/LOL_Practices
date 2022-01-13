7699a7e098b3e3ae021a268f6bffbbdd674f2103
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-01-13 13:42:58 -0300

    feat - el controller solo se ocupa de pedir y devolver, no maneja logica

--- back/src/controllers/champions.controller.js
+++ back/src/controllers/champions.controller.js
@@ -1,15 +1,6 @@
 const Champ = require('../models/championsModel')
-const { setAllChamps } = require('../services/champions')
 
 exports.allChamps = async (req, res) => {
 	const champs = await Champ.find({})
-
-	if (!champs[0]) {
-		const response = await setAllChamps()
-		console.log('no hay champs o.O, los seteamos.')
-		return res.json(response)
-	} else {
-		console.log('champs obtenidos de mongo "allChamps" in champs.controller')
-		return res.json(champs)
-	}
+	return res.json(champs)
 }
