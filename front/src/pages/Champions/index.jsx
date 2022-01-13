c06a92fa5393f7c01ebad95b2b4cef10bd4d55a7
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-01-13 20:09:58 -0300

    feat - add cgampions page

---
+++ front/src/pages/Champions/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ChampionCardsContainer } from '../../components/Champion/ChampionCardsContainer'
+
+export const Champions = () => {
+	return (
+		<div>
+			<ChampionCardsContainer />
+		</div>
+	)
+}
