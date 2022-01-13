dc0b85626365335b75d619ae98fd9123d553d447
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-01-13 20:02:24 -0300

    perf - add champions context

--- front/src/index.js
+++ front/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import LolApp from './LolApp'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ChampionsContextProvider } from './context/Champions/ChampionsContextProvider'
 
 ReactDOM.render(
 	<Router>
-		<LolApp />
+		<ChampionsContextProvider>
+			<LolApp />
+		</ChampionsContextProvider>
 	</Router>,
 	document.getElementById('root')
 )
