93ef86a5442a3e5a5699c4d8a13fdb61a81d16cf
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-01-14 16:00:52 -0300

    feat - add bacix styles to navigation

--- front/src/components/Navigation/index.jsx
+++ front/src/components/Navigation/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
+import styles from './styles.module.css'
+
 export const Navigation = () => {
 	return (
-		<div>
+		<div className={styles.navigation}>
 			<Link to={`/`}>Inicio</Link>
 			<Link to={`/champions`}>Champions</Link>
 		</div>
