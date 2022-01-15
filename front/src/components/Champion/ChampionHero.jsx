cc095e928da81495960d615680130e9ad50e767f
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-01-14 23:54:09 -0300

    feat - prepares hero component for champ detail view

--- front/src/components/Champion/ChampionHero.jsx
+++ front/src/components/Champion/ChampionHero.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
-export const ChampionHero = () => {
+
+import styles from './styles.module.css'
+
+export const ChampionHero = ({ images, lore, blurb, info }) => {
 	return (
-		<div>
-			<h1>Hero</h1>
-		</div>
+		<section>
+			<div className={styles.heroContainer}>
+				<img
+					className={styles.heroBackground}
+					src={images.splash[0]}
+					alt='champion-background-filtered'
+				/>
+
+				<img
+					className={styles.hero}
+					src={images.splash[0]}
+					alt='champion-background-filtered'
+				/>
+				<div className={styles.heroOverlay}></div>
+			</div>
+			<div className={styles.contentContainer}>
+				<div className={styles.contentRolContainer}>
+					<div className={styles.contentRol}>Rol</div>
+					<div className={styles.contentDiff}>{info.difficulty}</div>
+				</div>
+				<div className={styles.contentLore}>
+					{blurb || lore}
+					<button>ver mas...</button>
+				</div>
+			</div>
+		</section>
 	)
 }
