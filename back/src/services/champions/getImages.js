d75b15126d26af9e83f75897cfb25f09bea4826d
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-01-14 23:58:55 -0300

    perf - get images did not need to be asynchronous

--- back/src/services/champions/getImages.js
+++ back/src/services/champions/getImages.js
@@ -1,28 +1,52 @@
-const axios = require('axios')
-
 let version = '11.23.1'
-const _obtainSplashesUrl = async (champId, skines) => {
-	let toResponse = []
-	for (let skin of skines) {
-		let response = await axios
-			.get(
-				`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champId}_${skin.num}.jpg`
-			)
-			.catch((e) => {
-				const Error = {
-					custom: `an error ocurred making api request for a splash: ${skin.num} champ: ${champId}`,
-					status: e.status,
-					// message: e.message,
-					// error: e,
-				}
-				return Error
-			})
-		toResponse = [...toResponse, response.config?.url || 'undefined']
-	}
-	return Promise.all(toResponse) || response
+
+const getAllSplashesUrls = (champion) => {
+	const splashes = champion.skins.map(
+		(e) =>
+			`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_${e.num}.jpg`
+	)
+	return splashes
 }
 
-const _obtainLoadingUrl = async (champId, skines) => {
+const getAllLoadingUrls = (champion) => {
+	const loading = champion.skins.map(
+		(e) =>
+			`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_${e.num}.jpg`
+	)
+	return loading
+}
+
+const getAllSpellsUrls = (champion) => {
+	const spells = champion.spells.map(
+		(e) =>
+			`http://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${e.id}.png`
+	)
+	return spells
+}
+
+const getChampImages = (champion) => {
+	const splashes = getAllSplashesUrls(champion)
+	const loadings = getAllLoadingUrls(champion)
+	const spells = getAllSpellsUrls(champion)
+	const avatar = `http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.id}.png`
+	// avatar[url]
+
+	const passive = `http://ddragon.leagueoflegends.com/cdn/${version}/img/passive/${champion.passive.image.full}`
+	// passive[url]
+
+	const images = {
+		loadings,
+		avatar,
+		splashes,
+		passive,
+		spells,
+	}
+	return images
+}
+
+module.exports = { getChampImages }
+
+/* const _obtainLoadingUrl = async (champId, skines) => {
 	let toResponse = []
 	for (let skin of skines) {
 		let response = await axios
@@ -42,91 +66,4 @@ const _obtainLoadingUrl = async (champId, skines) => {
 	}
 
 	return Promise.all(toResponse) || response
-}
-
-const _obtainSpellsUrl = async (spells, champId) => {
-	let toResponse = []
-	for (let spell of spells) {
-		let response = await axios
-			.get(
-				`http://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${spell.id}.png`
-			)
-			.catch((e) => {
-				const Error = {
-					custom: `an error ocurred making api request for spell: ${spell.id} champ: ${champId}`,
-					status: e.status,
-					// message: e.message,
-					// error: e,
-				}
-				return Error
-			})
-		toResponse = [...toResponse, response.config?.url || 'undefined']
-	}
-	return Promise.all(toResponse) || response
-}
-
-const _obtainAvatarUrl = async (champId) => {
-	const response = await axios
-		.get(
-			`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champId}.png`
-		)
-		.catch((e) => {
-			const Error = {
-				custom: `an error ocurred making api request for champ avatar: ${champId}`,
-				status: e.status,
-				// message: e.message,
-				// error: e,
-			}
-			return Error
-		})
-
-	const url = response.config?.url || 'undefined'
-	return url
-}
-
-const _obtainPassiveUrl = async (passiveD, champId) => {
-	const response = await axios
-		.get(
-			`http://ddragon.leagueoflegends.com/cdn/${version}/img/passive/${passiveD}`
-		)
-		.catch((e) => {
-			const Error = {
-				custom: `an error ocurred making api request for champ passive: ${champId} (${passiveD})`,
-				status: e.status,
-				// message: e.message,
-				// error: e,
-			}
-			return Error
-		})
-
-	const url = response.config?.url || 'undefined'
-	return url
-}
-
-const getChampImages = async (champId, skines, spells, passiveD) => {
-	let splash = await _obtainSplashesUrl(champId, skines)
-	// splash[url]
-
-	let loading = await _obtainLoadingUrl(champId, skines)
-	// loading[url]
-
-	let spell = await _obtainSpellsUrl(spells, champId)
-	// spell[url]
-
-	const avatar = await _obtainAvatarUrl(champId)
-	// avatar[url]
-
-	const passive = await _obtainPassiveUrl(passiveD, champId)
-	// passive[url]
-
-	const images = {
-		loading,
-		avatar,
-		splash,
-		passive,
-		spell,
-	}
-	return images
-}
-
-module.exports = { getChampImages }
+} */
