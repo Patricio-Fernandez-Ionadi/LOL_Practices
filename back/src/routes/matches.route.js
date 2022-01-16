50d429ce32446d3792b74fb357b9fb708d74794d
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-01-16 19:34:59 -0300

    feat - current match now obtained from summoner route

--- back/src/routes/matches.route.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const matchesRoute = Router()
-
-const { currentMatch } = require('../controllers/matches.controller')
-
-matchesRoute.get('/current/:summonerName', currentMatch)
-
-module.exports = matchesRoute
--- back/src/routes/summoner.route.js
+++ back/src/routes/summoner.route.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const summonerRoute = Router()
 
+const { currentMatch } = require('../controllers/matches.controller')
 const {
 	summonerResume,
 	summonerHistory,
@@ -9,6 +10,7 @@ const {
 } = require('../controllers/summoner.controller')
 
 summonerRoute.get('/:summonerName', summonerResume)
+summonerRoute.get('/:summonerName/getcurrentgame', currentMatch)
 summonerRoute.get('/:summonerName/history', summonerHistory)
 summonerRoute.get('/:summonerName/history/:matchId', summonerMatch)
 summonerRoute.get('/admin/getAll', getAllSummoners)
