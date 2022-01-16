782cbb00b652f3df412fa65f92e1035641ce4bb6
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-01-16 16:41:46 -0300

    feat - add _createSummoner and _updateSummoner to get clear getSummonerByName()

--- back/src/services/summonerServices.js
+++ back/src/services/summonerServices.js
@@ -3,68 +3,68 @@ const axios = require('axios')
 const { headerRequest } = require('../helpers')
 const Summoner = require('../models/summonerModel')
 
-const _getSummonerStats = async (userId) => {
-	const statsResponse = await axios
+/**
+ *
+ * @param {String} summonerId id del summoner recibido en la busqueda por nombre
+ * @returns object {
+ * leagueId,
+ * summonerId,
+ * summonerName,
+ * queueType,
+ * tier,
+ * rank,
+ * leaguePoints,
+ * wins,
+ * losses,
+ * hotStreak,
+ * veteran,
+ * freshBlood,
+ * inactive
+ * }
+ */
+const _getSummonerStatsLeague = async (summonerId) => {
+	// console.log('_getSummonerStatsLeague called')
+	const { data } = await axios
 		.get(
-			`${process.env.LAS_URI}/lol/league/v4/entries/by-summoner/${userId}`,
+			`${process.env.LAS_URI}/lol/league/v4/entries/by-summoner/${summonerId}`,
 			headerRequest
 		)
-		.catch((e) => ({
-			_getsummonerstats: {
-				status: e.status,
-				data: e.data,
-				error: e,
-			},
-		}))
-	return statsResponse.data
+		.catch((e) => {
+			console.log(e.status, e)
+		})
+	return data
 }
 
-const getSummonerByName = async (summonerName) => {
-	const [summoner] = await Summoner.find({ name: summonerName })
-
-	if (summoner?.id && summoner?.stats?.leagueId) {
-		console.log('summoner from mongo 1st try')
-		// console.log(summoner)
-		return summoner
-	} else if (summoner && !summoner?.stats?.leagueId) {
-		const [stats] = await _getSummonerStats(summoner.id)
-
-		if (stats) {
-			const newInfo = {
-				...summoner,
-				stats: {
-					leagueId: stats.leagueId,
-					queueType: stats.queueType,
-					rank: stats.rank,
-					leaguePoints: stats.leaguePoints,
-					wins: stats.wins,
-					losses: stats.losses,
-					veteran: stats.veteran,
-					inactive: stats.inactive,
-					freshBlood: stats.freshBlood,
-					hotStreak: stats.hotStreak,
-				},
-			}
-
-			const summToUpdate = await Summoner.findOneAndUpdate(
-				{ id: summoner.id },
-				newInfo,
-				{ new: true }
-			)
-
-			console.log('updated summoner in mongo')
-			return summToUpdate
-		} else {
-			console.log('user returned from mongo, still without stats')
-			// console.log(summoner)
-			return summoner
-		}
-	}
-
-	const sumNameForRequest = encodeURI(summonerName)
-	const { data: summonerIds } = await axios
+const _getSummonerStatsTft = async (summonerId) => {
+	const { data } = await axios
 		.get(
-			`${process.env.LAS_URI}/lol/summoner/v4/summoners/by-name/${sumNameForRequest}`,
+			`${process.env.LAS_URI}/tft/league/v1/entries/by-summoner/${summonerId}`,
+			headerRequest
+		)
+		.catch((e) => {
+			console.log(e.status, e)
+		})
+	return data
+}
+
+/**
+ * 
+ * @param {String} summonerName summoner name prepared for query
+ * @returns {
+  id ,
+  accountId,
+  puuid,
+  name,
+  profileIconId,
+  revisionDate,
+  summonerLevel
+}
+ */
+const _getSummonerByName = async (summonerName) => {
+	// console.log('_getSummonerByName called')
+	const { data } = await axios
+		.get(
+			`${process.env.LAS_URI}/lol/summoner/v4/summoners/by-name/${summonerName}`,
 			headerRequest
 		)
 		.catch((e) => {
@@ -76,11 +76,27 @@ const getSummonerByName = async (summonerName) => {
 				},
 			}
 		})
+	return data
+}
 
-	// console.log(summonerIds)
-	const { id: userId } = summonerIds
+const _getSummonerHistoryLeague = async (puuid) => {
+	// console.log('_getSummonerHistoryLeague called')
+	const { data } = await axios
+		.get(
+			`${process.env.LOL_REGION}/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20`,
+			headerRequest
+		)
+		.catch((err) => res.json(err))
 
-	const [newStats] = await _getSummonerStats(userId)
+	return data
+}
+
+const _createNewSummoner = async (summonerName) => {
+	// console.log('_createNewSummoner called')
+	const sumNameForRequest = encodeURI(summonerName)
+	const summonerIds = await _getSummonerByName(sumNameForRequest)
+	const { id } = summonerIds
+	const [newStats] = await _getSummonerStatsLeague(id)
 
 	if (!newStats) {
 		const newSummoner = new Summoner({
@@ -92,7 +108,7 @@ const getSummonerByName = async (summonerName) => {
 			revisionDate: summonerIds.revisionDate,
 			summonerLevel: summonerIds.summonerLevel,
 		})
-		console.log('user created without stats')
+		console.log('summoner created without stats')
 		await newSummoner.save()
 		const createdSummoner = await Summoner.find({
 			id: summonerIds.id,
@@ -112,6 +128,7 @@ const getSummonerByName = async (summonerName) => {
 				queueType: newStats.queueType,
 				rank: newStats.rank,
 				leaguePoints: newStats.leaguePoints,
+				tier: newStats.tier,
 				wins: newStats.wins,
 				losses: newStats.losses,
 				veteran: newStats.veteran,
@@ -120,7 +137,7 @@ const getSummonerByName = async (summonerName) => {
 				hotStreak: newStats.hotStreak,
 			},
 		})
-		console.log('user created with stats')
+		console.log('summoner created with stats')
 
 		await newSummoner.save()
 		const createdSummoner = await Summoner.find({
@@ -131,18 +148,72 @@ const getSummonerByName = async (summonerName) => {
 	}
 }
 
-const getSummonerHistory = async (puuid) => {
-	const { data } = await axios
-		.get(
-			`${process.env.LOL_REGION}/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20`,
-			headerRequest
-		)
-		.catch((err) => res.json(err))
+const _updateSummonerStatsLeague = async (summonerId) => {
+	// console.log('_updateSummonerStatsLeague called')
+	const [summoner] = await Summoner.find({ id: summonerId })
+	const [stats] = await _getSummonerStatsLeague(summonerId)
 
-	return data
+	if (stats) {
+		const newInfo = {
+			...summoner,
+			stats: {
+				leagueId: stats.leagueId,
+				queueType: stats.queueType,
+				rank: stats.rank,
+				tier: stats.tier,
+				leaguePoints: stats.leaguePoints,
+				wins: stats.wins,
+				losses: stats.losses,
+				veteran: stats.veteran,
+				inactive: stats.inactive,
+				freshBlood: stats.freshBlood,
+				hotStreak: stats.hotStreak,
+			},
+		}
+
+		const summToUpdate = await Summoner.findOneAndUpdate(
+			{ id: summoner.id },
+			newInfo,
+			{ new: true }
+		)
+
+		console.log('updated summoner in mongo')
+		return summToUpdate
+	} else {
+		console.log('summoner returned still without stats')
+		// console.log(summoner)
+		return summoner
+	}
+}
+// ---------------------------------------------------------------------
+
+/**
+ *
+ * @param {String} summonerName summoner name
+ * - try to get the summoner with full info from mongo
+ * - in case summoner exist but has no information form stats, try to update that info
+ * - in case summoner does not exist, create it
+ * @returns summoner object with ids and maybe stats... xD
+ */
+const getSummonerByName = async (summonerName) => {
+	// console.log('getSummonerByName called')
+	const [summoner] = await Summoner.find({ name: summonerName })
+	if (summoner?.id && summoner?.stats?.leagueId) {
+		console.log('summoner from mongo 1st try')
+		return summoner
+	} else if (summoner && !summoner?.stats?.leagueId) {
+		console.log('summoner being updated')
+		const updatedSummoner = await _updateSummonerStatsLeague(summoner.id)
+		return updatedSummoner
+	} else if (!summoner || !summoner.id) {
+		console.log('summoner being created')
+		const newSummoner = await _createNewSummoner(summonerName)
+		return newSummoner
+	}
 }
 
 // ---------------------------------------------------------------------
+// create match teams from a past match id
 const matchTeams = async (matchId) => {
 	const { data: info } = await axios.get(
 		`${process.env.LOL_REGION}/lol/match/v5/matches/${matchId}`,
@@ -177,6 +248,7 @@ const matchTimeLine = async (matchId) => {
 	return data
 }
 
+// get a specific past match
 const getSummonerMatch = async (matchId) => {
 	const timeLine = await matchTimeLine(matchId)
 	const { blueTeam, redTeam, info } = await matchTeams(matchId)
@@ -185,4 +257,4 @@ const getSummonerMatch = async (matchId) => {
 	return { teams, timeLine, info }
 }
 
-module.exports = { getSummonerByName, getSummonerHistory, getSummonerMatch }
+module.exports = { getSummonerByName, getSummonerMatch }
