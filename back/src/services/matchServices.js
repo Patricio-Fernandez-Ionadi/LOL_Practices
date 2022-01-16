6545392e4854124dd41d637b7e417d393069fd19
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-01-16 16:40:28 -0300

    feat - getCurrentMatch exported as named func

--- back/src/services/matchServices.js
+++ back/src/services/matchServices.js
@@ -1,23 +1,21 @@
 const axios = require('axios')
-
 const { headerRequest } = require('../helpers')
 
-exports.getMatch = {
-	current: async (summId) => {
-		const { data } = await axios
-			.get(
-				`${process.env.LAS_URI}/lol/spectator/v4/active-games/by-summoner/${summId}`,
-				headerRequest
-			)
-			.catch((e) => {
-				return {
-					currentBysummonerId: {
-						status: e.response.status,
-						error: e,
-					},
-				}
-			})
-		// console.log(data)
-		return data
-	},
+const getCurrentMatch = async (summId) => {
+	const { data } = await axios
+		.get(
+			`${process.env.LAS_URI}/lol/spectator/v4/active-games/by-summoner/${summId}`,
+			headerRequest
+		)
+		.catch((e) => {
+			return {
+				currentBysummonerId: {
+					status: e.response.status,
+					error: e,
+				},
+			}
+		})
+	// console.log(data)
+	return data
 }
+module.exports = { getCurrentMatch }
